03dc87c582cdf3c5fb2638b814025ed71eb13c0f
Walter Doerwald <walter@livinglogic.de>
2021-07-08 10:33:45 +0200

    Add copyright notice.

--- src/pygll.py
+++ src/pygll.py
@@ -1,3 +1,26 @@
+# Copyright 2021 by LivingLogic AG, Bayreuth/Germany
+# Copyright 2021 by Walter Dörwald
+#
+# All Rights Reserved
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+# THE SOFTWARE.
+
 from pygments import token, style
 
 
@@ -18,10 +41,10 @@ class LivingLogicLightStyle(style.Style):
 	# line number background color
 	line_number_background_color = 'transparent'
 
-	# special line number font color
+	# special line number font color (``:emphasize-lines`` in Sphinx code blocks)
 	line_number_special_color = '#5d5d5d'
 
-	# special line number background color
+	# special line number background color (``:emphasize-lines`` in Sphinx code blocks)
 	line_number_special_background_color = 'transparent'
 
 	styles = {
@@ -80,10 +103,10 @@ class LivingLogicDarkStyle(style.Style):
 	# line number background color
 	line_number_background_color = 'transparent'
 
-	# special line number font color
+	# special line number font color (``:emphasize-lines`` in Sphinx code blocks)
 	line_number_special_color = '#a0a0a0'
 
-	# special line number background color
+	# special line number background color (``:emphasize-lines`` in Sphinx code blocks)
 	line_number_special_background_color = 'transparent'
 
 	styles = {
